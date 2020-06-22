d84212de296656342463196b59bd5edbb523bb54
leo <leocarminhoto@gmail.com>
2020-06-22 13:43:00 -0300

    HOOKS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Spinner from './components/Spinner';
 import Cadidates from './components/Cadidates';
 
-export default class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      candidates: [],
-    }
-    this.interval = null
-  }
-   componentDidMount(){
-    this.interval = setInterval(()=>{
+export default function App() {
+  const [candidates, setCandidates] = useState([])
+  //const [intervall, setIntervall ]= useState(null)
+  const interval = null
+  
+  useEffect(() => {
+    
+    const interval = setInterval(()=>{
       fetch('http://localhost:8080/votes').then(res=>{
         return res.json()
       }).then(json =>{
-        this.setState({
-          candidates: json.candidates
-        })
+        setCandidates(json.candidates)
       })
     },1000)
-  }
-  render() {
-    const {candidates} = this.state
+    
+  }, [])
     console.log(candidates)
     if (candidates.length === 0 ){
       return <Spinner description="Carregando..."/>
@@ -34,5 +29,4 @@ export default class App extends Component {
         <Cadidates candidates={candidates}/>
       </div>
     )  
-  }
 }
